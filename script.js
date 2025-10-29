0e0d14c39f6983f11aa5eaac55ba7eb2fe73c329
Felice Louise <153695719+Louklin@users.noreply.github.com>
2025-10-29 10:34:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+// script.js
+function showMessage() {
+    const p = document.getElementById('message');
+    if (p) {
+        p.textContent = "The JS is running and the UI is under review!" // OMITTING SEMICOLON (The intentional error)
+    }
+}
+
+showMessage();
