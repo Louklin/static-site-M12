ff01e4d4817adc77a3b7393be61afc21c4ace8ce
Felice Louise <153695719+Louklin@users.noreply.github.com>
2025-11-05 20:56:58 +0800

    Update demoFail.test.js

--- tests/demoFail.test.js
+++ tests/demoFail.test.js
@@ -1,4 +1,4 @@
 // Demonstration of a failing test (skipped by default). Remove .skip to see CI fail.
-test.skip('intentional failing demo', () => {
+test('intentional failing demo', () => {
   expect(1 + 1).toBe(3);
 });
