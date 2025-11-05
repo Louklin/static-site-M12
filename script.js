3a8f9c4739d0f2aee9f8113c566b537e4a869b60
Felice Louise <153695719+Louklin@users.noreply.github.com>
2025-11-05 10:10:21 +0800

    Add function to change background color
    Added a function to change the background color randomly.

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ function showMessage() {
     }
 }
 
+function changeBackgroundColor() {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+}
 showMessage();
